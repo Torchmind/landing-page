87bd7aafcb32f4f9b1ad0d6075b01e917c03d8b1
Johannes Donath <johannesd@torchmind.com>
2016-04-25 22:00:29 +0200

    Added a basic representation for 2D dimensions.

---
+++ src/script/firefly/space/Dimensions.ts
@@ -0,0 +1,137 @@
+/*
+ * Copyright 2016 Johannes Donath <johannesd@torchmind.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import {Cloneable} from "../utility/Cloneable";
+
+/**
+ * Dimensions
+ *
+ * Provides a representation of the absolute dimensions of an element within or outside of a container such as an HTML 5
+ * canvas container.
+ *
+ * @author <a href="mailto:johannesd@torchmind.com">Johannes Donath</a>
+ */
+export class Dimensions implements Cloneable {
+        private _height : number;
+        private _width : number;
+        private _listener : (width, height) => boolean;
+
+        public constructor(width : number, height : number, listener : (width, height) => boolean = null) {
+                this._width = width;
+                this._height = height;
+                this._listener = listener;
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        public clone() : Dimensions {
+                return new Dimensions(this._width, this._height);
+        }
+
+        /**
+         * Checks whether the supplied set of dimensions are equal to the dimensions represented by this instance.
+         *
+         * @param dimensions a set of dimensions to compare to.
+         * @returns {boolean} true if equal, false otherwise.
+         */
+        public equals(dimensions : Dimensions) : boolean {
+                if (dimensions == null || dimensions == undefined) {
+                        return false;
+                }
+
+                return this == dimensions || (this._width == dimensions.height && this._height == dimensions.height);
+        }
+
+        /**
+         * Converts this instance into a human readable string which may be used for logging purposes.
+         * @returns {string} a string representation.
+         */
+        public toString() : string {
+                return this._width + 'x' + this._height;
+        }
+
+        /**
+         * Updates the width and height of this instance using the values provided by the supplied set of dimensions.
+         *
+         * Note: This method returns a reference to its parent instance and is thus chain-able.
+         *
+         * @param dimensions an instance to pull the new values from.
+         * @returns {Dimensions} a reference to this instance.
+         */
+        public update(dimensions : Dimensions) : Dimensions {
+                if (!this.equals(dimensions) && this._listener != null) {
+                        if (!this._listener(dimensions.width, dimensions.height)) {
+                                return this;
+                        }
+                }
+
+                this._width = dimensions.width;
+                this._height = dimensions.height;
+
+                return this;
+        }
+
+        /* == Getters & Setters == */
+
+        /**
+         * Retrieves the current element height.
+         *
+         * @returns {number} a height.
+         */
+        get height() : number {
+                return this._height;
+        }
+
+        /**
+         * Sets the current element height.
+         *
+         * @param value a new height.
+         */
+        set height(value : number) {
+                if (this._height != value && this._listener != null) {
+                        if (!this._listener(value, this._height)) {
+                                return;
+                        }
+                }
+
+                this._height = value;
+        }
+
+        /**
+         * Retrieves the current element width.
+         *
+         * @returns {number} a width.
+         */
+        get width() : number {
+                return this._width;
+        }
+
+        /**
+         * Sets the current element width.
+         *
+         * @param value a new width.
+         */
+        set width(value : number) {
+                if (this._width != value && this._listener != null) {
+                        if (!this._listener(this._width, value)) {
+                                return;
+                        }
+                }
+
+                this._width = value;
+        }
+}
